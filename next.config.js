6bc2fab1cdeae8c1ca81177f20108c667d55d4cc
Temel00 <temelaudio@gmail.com>
2024-08-12 21:47:02 -0700

    initial

---
+++ next.config.js
@@ -0,0 +1,23 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',  
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    APP_VERSION: process.env.npm_package_version,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ];
+  },
+};
+
+module.exports = withPWA(nextConfig);
